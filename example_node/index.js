2322ecc19e7d7073c6cf66efb1c2f1cb39f17628
EightDoor <851708184@qq.com>
2022-05-13 09:40:12 +0800

    🐳 chore(eslint):

--- example_node/index.js
+++ example_node/index.js
@@ -1,23 +1,23 @@
-const { LogUtil } = require("zhoukai_utils");
+const { LogUtil } = require('zhoukai_utils')
 
-const log = new LogUtil();
+const log = new LogUtil()
 
-log.d("content test", "debug test");
-log.i("content test", "info test");
-log.e("content test", "error test");
-log.s("content test123", "success test");
+log.d('content test', 'debug test')
+log.i('content test', 'info test')
+log.e('content test', 'error test')
+log.s('content test123', 'success test')
 
 const info = {
   test1: 1,
   test2: 2,
-};
-log.d(info, "content object debug");
-log.s(info, "success test");
+}
+log.d(info, 'content object debug')
+log.s(info, 'success test')
 
-log.startTime("业务");
+log.startTime('业务')
 setTimeout(() => {
-  log.d("执行完成");
-}, 2000);
+  log.d('执行完成')
+}, 2000)
 
 // import { LogUtil } from "zhoukai_utils";
 
